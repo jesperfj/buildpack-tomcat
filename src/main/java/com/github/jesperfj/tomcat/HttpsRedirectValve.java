b174a2f19f69ee5f2e90773a7bbfb4ee2bd4e7fe
Jesper Joergensen <jesper@heroku.com>
2012-08-11 15:30:19 -0700

    https redirect valve should sort of work now

--- src/main/java/com/github/jesperfj/tomcat/HttpsRedirectValve.java
+++ src/main/java/com/github/jesperfj/tomcat/HttpsRedirectValve.java
@@ -7,21 +7,22 @@ import javax.servlet.ServletException;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
 
 
 public class HttpsRedirectValve extends ValveBase {
 
-    private static final Log log = LogFactory.getLog(HttpsRedirectValve.class);
-
     @Override
     public void invoke(Request request, Response response) throws IOException, ServletException {
-        if("true".equals(System.getProperty("https_redirect"))) {
-            final String proto = request.getHeader("X-Forwarded-Proto");
-            log.info("Protocol: "+proto);
+        if("true".equals(System.getProperty("https_redirect")) &&
+           !"https".equals(request.getHeader("X-Forwarded-Proto"))) {
+                StringBuffer uri = request.getRequestURL();
+                if(request.getQueryString()!=null) {
+                    uri.append("?"+request.getQueryString());
+                }
+                response.sendRedirect("https"+uri.substring(4));
+        } else {
+            next.invoke(request, response);
         }
-        
     }
 
 }
